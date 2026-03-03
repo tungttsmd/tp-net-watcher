bfc0de87ab4d86b94b662f14928106f2d52ed1cd
thanhtung.tran2k <thanhtung.tran2k@gmail.com>
2026-03-03 13:41:08 +0700

    tp-net-watcher-0.0.1

--- build_winsv_wtpsvn.bat
+++ build_winsv_wtpsvn.bat
@@ -118,7 +118,9 @@ if exist "%ENV_SRC%" (
     echo %GREEN%[OK]%RESET% .env sao chep thanh cong
 ) else (
     echo %YELLOW%[WARN]%RESET% Khong tim thay .env tai: %ENV_SRC%
-    echo %YELLOW%[WARN]%RESET% Hay tu dat .env vao: "%APP_DIR%\app\"
+    echo %YELLOW%[WARN]%RESET% Hay tu dat .env vao: 
+    echo		tp-net-watcher\
+    echo         ^|-- app\%GREEN%.env%RESET%
 )
 
 set "RUN_SRC=%ROOT_DIR%batFile\run.bat"
